91dad12806e8643971cd5034d61d34f471c896b9
Williamlo844 <129482583+Williamlo844@users.noreply.github.com>
2023-11-13 16:00:35 +0700

    profil

---
+++ resources/views/user/profil.blade.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link
+      href="https://fonts.googleapis.com/css?family=DM+Serif+Text&display=swap"
+      rel="stylesheet"
+    />
+    <link href="./css/main.css" rel="stylesheet" />
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="v588_152">
+      <div class="v649_81">
+        <div class="v649_82"></div>
+        <div class="v649_83"></div>
+        <div class="v649_84"></div>
+        <div class="v649_85">
+          <div class="v649_86"></div>
+          <span class="v649_87">(021) 5595 3449</span
+          ><span class="v649_88"
+            >Taman Palem Lestari Blok B18/26 Cengkareng, Jakarta Barat</span
+          ><span class="v649_89">KB - TK PELITA KASIH</span>
+          <div class="v649_90"></div>
+        </div>
+        <div class="v649_91">
+          <span class="v649_92">SMP - SMA PELITA KASIH</span>
+          <div class="v649_93"></div>
+          <span class="v649_94">(021) 5595 2526</span
+          ><span class="v649_95"
+            >Taman Palem Lestari Blok E9/1 Cengkareng, Jakarta Barat</span
+          >
+          <div class="v649_96"></div>
+        </div>
+        <div class="v649_97">
+          <span class="v649_98">SD PELITA KASIH</span>
+          <div class="v649_99"></div>
+          <span class="v649_100">(021) 5595 2655</span
+          ><span class="v649_101"
+            >Taman Palem Lestari Blok E9/1 Cengkareng, Jakarta Barat</span
+          >
+          <div class="v649_102"></div>
+        </div>
+        <span class="v649_103">INFORMASI PENDAFTARAN </span
+        ><span class="v649_104"
+          >@ 2023 Copyright Sekolah Pelita Kasih Jakarta</span
+        >
+        <div class="v649_105"></div>
+        <div class="v649_106"></div>
+      </div>
+      <div class="v649_76">
+        <div class="v659_18"></div>
+        <div class="v659_17"></div>
+        <div class="v659_19"></div>
+        <div class="v659_20"></div>
+        <span class="v649_109"
+          >Terwujudnya siswa yang UNGGUL, SEHAT jasmani dan rohani, memiliki
+          KARAKTER POSITIF, CITRA DIRI POSITIF, memiliki tujuan hidup yang
+          jelas, memiliki STRATEGI DAN KOMITMEN dalam menggapai cita-
+          citanya.</span
+        ><span class="v649_110"
+          >Menanamkan pendidikan berkarakter. Membimbing peserta didik yang
+          selalu optimis, percaya diri, berpikiran positif, mengetahui kelebihan
+          pada dirinya yang bermanfaat untuk orang lain dan mempunyai gambaran
+          yang jelas tentang masa depannya. Membina peserta didik yang bertumbuh
+          menjadi siswa yang sehat, berbudi pekerti luhur dan bertanggung jawab
+          sesuai nilai kebenaran. Memperlengkapi peserta didik dengan
+          keterampilan yang berkualitas sesuai kebutuhan dan untuk pengembangan
+          dirinya.</span
+        ><span class="v649_107">Visi</span><span class="v649_108">Misi</span>
+        <div class="v663_24"></div>
+        <div class="v666_31"></div>
+        <div class="v666_30"></div>
+        <div class="v668_33"></div>
+      </div>
+      <div class="v649_75">
+        <div class="v649_74"></div>
+        <span class="v624_17"
+          >Sekolah Pelita Kasih menggunakan kurikulum nasional dan menerapkan
+          nilai-nilai Kristiani. Kami percaya bahwa setiap individu adalah unik
+          dan istimewa dengan kemampuan untuk mengembangkan potensi yang
+          dimikilinya dan menjadi yang terbaik.</span
+        >
+        <div class="v649_78"></div>
+        <span class="v649_79"
+          >Sistem pendidikan, staff pengajar yang profesional dan berdedikasi
+          serta lingkungan sekolah yang mengayomi dapat mendukung dan
+          menumbuhkan potensi terbaik setiap individu untuk menjadi seseorang
+          unggul, berpengetahuan, percaya kepada Tuhan, sehat jasmani, memiliki
+          karakter positif, citra diri positif, memiliki tujuan hidup yang
+          jelas, memiliki strategi dan komitmen serta keahlian untuk menjalani
+          kehidupan dan menggapai cita- citanya.</span
+        >
+        <div class="v649_77"></div>
+        <span class="v624_14">Tentang Kami</span>
+      </div>
+      <div class="v624_13">
+        <div class="v622_6"></div>
+        <span class="v624_8">PROFIL</span>
+      </div>
+      <div class="v588_153">
+        <div class="v588_154">
+          <div class="v588_156">
+            <span class="v588_157">Beranda</span
+            ><span class="v588_158">Fasilitas</span
+            ><span class="v588_159">Akademik</span
+            ><span class="v588_160">Profil</span
+            ><span class="v588_161">Struktur Sekolah</span
+            ><span class="v588_162">Ekstrakulikuler</span>
+            <div class="v588_165"></div>
+            <div class="v588_166"></div>
+            <div class="v588_167"></div>
+            <div class="v588_168"></div>
+            <div class="v588_169"></div>
+            <div class="v506_45"></div>
+          </div>
+        </div>
+        <div class="v648_56">
+          <div class="v648_57"></div>
+          <span class="v648_58">Contact Us : </span>
+          <div class="v648_59"></div>
+          <span class="v648_60">+6285156622908</span
+          ><span class="v648_61">pelitakasih@gmail.com</span>
+          <div class="v648_62"></div>
+        </div>
+      </div>
+      <div class="v659_21">
+        <div class="v659_12"></div>
+        <span class="v659_13">Tentang Kami</span
+        ><span class="v659_14">Visi & Misi</span>
+      </div>
+      <div class="v763_25"></div>
+    </div>
+  </body>
+</html>
